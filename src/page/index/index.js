35c81f843d3da7af3f7300ef551c8d4854671565
gandazhi <18228438700@163.com>
2017-10-26 22:55:39 +0800

    对接首页接口和搜索页接口
    首页接口暂时还没有想到好的方法去循环遍历操作dom对象，绑定数据

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -3,26 +3,45 @@ require('page/common/nav/index.js');
 require('page/common/header/index.js');
 require('util/slider/index.js');
 require('./index.css');
-var htmlTemplte = require('./index.string');
+var htmlTemplate = require('./index.string');
+var floorTemplate = require('./floor.string');
 var _mm = require('util/mm.js');
+var _index = require('service/index-service');
 
-$(function() {
+$(function () {
     page.init();
     $('.banner').unslider({
         // dots: true,
     });
-    $('.banner-con .banner-arrow').click(function(){
+    $('.banner-con .banner-arrow').click(function () {
         var forward = $(this).hasClass('prev') ? 'prev' : 'next';
         $slider.data('unslider')[forward]();
     });
 });
 
 var page = {
-    init : function () {
+    init: function () {
         var html = '';
-        html = _mm.renderHtml(htmlTemplte, '');
+        html = _mm.renderHtml(htmlTemplate, '');
         $('.banner-con').html(html);
-    }
-}
+        this.getProductRecommend();
+    },
+    getProductRecommend: function () {
+
+        var categoryIdArray = new Array('100001', '100002');
+        var floorClass = new Array('floor1','floor2')
+        var data = {
+            categoryId: categoryIdArray[1],
+            num: _mm.getUrlParam('num') || '10'
+        };
+        _index.getProductRecommend(data, function (res) {
+            //渲染模板
+            var floorHtml = _mm.renderHtml(floorTemplate, {data: res});
+            $('.floor-warp').html(floorHtml);
+        }, function (errMsg) {
+            _mm.errorTips(errMsg);
+        });
+    },
+};
 
 
--- src/page/list/index.js
+++ src/page/list/index.js
@@ -1,7 +1,45 @@
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
+require('page/index/index');
 require('./index.css');
 
-var navSide = require('page/common/nav-side/index.js');
 var _mm = require('util/mm.js');
 var _user = require('service/user-service.js');
+var _product = require('service/product-service');
+var htmlTemplate = require('./index.string');
+
+var page = {
+    data: {
+        listParam: {
+            keywords: _mm.getUrlParam('keywords') || '',
+            orderBy: _mm.getUrlParam('orderBy') || 'sales_volume-desc',
+            pageNum: _mm.getUrlParam('pageNum') || 1,
+            pageSize: _mm.getUrlParam('pageSize') || 20,
+        }
+    },
+    init: function () {
+        this.onLoad();
+        this.bindEvent();
+    },
+    bindEvent: function () {
+
+    },
+    onLoad: function () {
+        this.loadList();
+    },
+    loadList: function () {
+        var listParam = this.data.listParam;
+        _product.searchProductList(listParam, function (res) {
+            var html = _mm.renderHtml(htmlTemplate, {
+                list: res.list
+            });
+            $('.p-list-con').html(html);
+        }, function (errMsg) {
+            _mm.errorTips(errMsg);
+        });
+    },
+};
+
+$(function () {
+    page.init();
+});
